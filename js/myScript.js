23afa668e84f759f61490c2ffec8581ef3ce444f
Edith Barry <eblinnb@earthlink.net>
2018-04-25 16:17:34 +0000

    first upload

---
+++ js/myScript.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+$("#year").mouseenter(function(){
+    alert("If you know where to get a yearly list, please let me know!");
+});
+});
+
+
+$(document).ready(function(){
+$("#year").mouseleave(function(){
+    alert("Send an email to abc@defg.com. Thanks!");
+});
+});
